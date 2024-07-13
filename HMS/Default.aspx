2d43136ec1c7473534e75138c23cacce42be0e2f
Santhuns2001 <n.s.santhosh116@gmail.com>
2024-07-13 18:58:55 +0530

    completed some tasks on given Task-Project

---
+++ HMS/Default.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HMS._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+
+        <h1>Hospital Bill</h1>
+    <p>
+        <asp:Label ID="Label1" runat="server" Text="Bill Number"></asp:Label>
+        <asp:TextBox ID="TextBox1" Visible="true" runat="server" readonly="true"></asp:TextBox>
+        <asp:DropDownList ID="DropDownList3" runat="server" Visible="false" 
+            AutoPostBack="True" OnSelectedIndexChanged="ddlBillIDs_SelectedIndexChanged"
+            Width="200px">
+        </asp:DropDownList>
+        <asp:Label ID="Label2" runat="server" Text="Bill Date"></asp:Label>
+        <asp:TextBox ID="TextBox2" type="date" runat="server" readonly="true"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Label ID="Label3" runat="server" Text="Patient Name"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" ReadOnly="true"></asp:TextBox>
+        <asp:Label ID="Label4" runat="server" Text="Gender"></asp:Label>
+        <asp:DropDownList ID="DropDownList1" runat="server" Enabled="false">
+            <asp:ListItem></asp:ListItem>
+            <asp:ListItem>Male</asp:ListItem>
+            <asp:ListItem>Female</asp:ListItem>
+        </asp:DropDownList>
+        <asp:Label ID="Label5" runat="server" Text="Date of Birth"></asp:Label>
+        <asp:TextBox ID="TextBox4" type="date" runat="server" ReadOnly="true"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Label ID="Label6" runat="server" Text="Address"></asp:Label>
+        <asp:TextBox ID="TextBox5" runat="server" readonly="true"></asp:TextBox>
+        <asp:Label ID="Label7" runat="server" Text="Email Id"></asp:Label>
+        <asp:TextBox ID="TextBox7" runat="server" readonly="true"></asp:TextBox>
+        <asp:Label ID="Label8" runat="server" Text="Mobile Number"></asp:Label>
+        <asp:TextBox ID="TextBox6" runat="server" readonly="true"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Label ID="Label9" runat="server" Text="Investigations"></asp:Label>
+        <asp:DropDownList ID="DropDownList2" runat="server" Enabled="false" Width="200px" 
+          AutoPostBack="true"  OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+        </asp:DropDownList>
+        <asp:Label ID="Label10" runat="server" Text="Fee"></asp:Label>
+        <asp:TextBox ID="TextBox8" runat="server" readOnly="true" ></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Add To Grid" Enabled="false" OnClick="AddToGrid" />
+    </p>
+    <p>
+        <%--OnSelectedIndexChanged="GridView1_SelectedIndexChanged"--%>
+        <asp:GridView style="border:solid 5px red; " ID="GridView1" runat="server" ShowHeaderWhenEmpty="true"
+            OnRowDataBound="GridView1_RowDataBound" CellPadding="4" ForeColor="#333333" GridLines="None">
+
+            <AlternatingRowStyle BackColor="White" />
+
+            <Columns>
+                    <asp:TemplateField HeaderText="SNo">
+                        <ItemTemplate>
+                            <asp:Label ID="lblSerialNumber" runat="server"></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    
+            </Columns>
+
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+
+        </asp:GridView>
+
+<%--        <asp:GridView ID="GridView1" runat="server"
+    AutoGenerateColumns="False"
+    OnRowDataBound="GridView1_RowDataBound"
+    BorderColor="Black"
+    BorderStyle="Solid"
+    BorderWidth="2px"
+    CellPadding="4"
+    CellSpacing="1"
+    GridLines="Both"
+    style="border:solid 5px red;">
+    <HeaderStyle BackColor="#0033CC" ForeColor="White" Font-Bold="True" />
+    <RowStyle BackColor="White" ForeColor="Black" />
+    <AlternatingRowStyle BackColor="#f2f2f2" ForeColor="Black" />
+    <SelectedRowStyle BackColor="#FFCC66" ForeColor="Black" Font-Bold="True" />
+    <PagerStyle BackColor="#0066CC" ForeColor="White" HorizontalAlign="Center" />
+    <EditRowStyle BackColor="#FFFFCC" ForeColor="Black" />
+    <FooterStyle BackColor="#0033CC" ForeColor="White" Font-Bold="True" />
+    <EmptyDataTemplate>
+        <div style="text-align:center; color:red; font-weight:bold;">No records found.</div>
+    </EmptyDataTemplate>
+    <Columns>
+        <asp:BoundField DataField="Investigation_Name" HeaderText="Investigation_Name" SortExpression="Investigation_Name" />
+        <asp:BoundField DataField="Fees" HeaderText="Fees" SortExpression="Fees" />
+        
+    </Columns>
+</asp:GridView>--%>
+
+
+    </p>
+    <p>
+        <asp:Button ID="Button2" runat="server" Enabled = "true" Text="Add" OnClick="Add" />
+        <asp:Button ID="Button3" runat="server" Text="Edit" OnClick="Edit" />
+        <asp:Button ID="Button4" runat="server" Enabled = "false" Text="Save" OnClick="Save" />
+        <asp:Button ID="Button5" runat="server" Text="Clear" OnClick="Clear" />
+    </p>
+    <p>&nbsp;</p>
+       
+    </main>
+
+</asp:Content>
---
+++ HMS/Default.aspx.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HMS
+{
+    public partial class _Default : Page
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DELL\SQLEXPRESS;Initial Catalog=HospitalManagementSystemDb;Integrated Security=True;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillDrop();
+            }
+        }
+
+        private void FillDrop()
+        {
+            sqlConnection.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from Investigation", sqlConnection);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            DropDownList2.DataSource = dt;
+            DropDownList2.DataTextField = "Investigation_Name";
+            DropDownList2.DataValueField = "Invest_Id";
+            DropDownList2.DataBind();
+            sqlConnection.Close();
+        }
+
+        void EnableTags()
+        {
+            TextBox2.ReadOnly = TextBox3.ReadOnly = 
+                TextBox4.ReadOnly= TextBox5.ReadOnly = TextBox7.ReadOnly = TextBox6.ReadOnly = false;
+            DropDownList1.Enabled = DropDownList2.Enabled = Button1.Enabled = true;
+        }
+
+        void DisableTags()
+        {
+            TextBox2.ReadOnly = TextBox3.ReadOnly = TextBox4.ReadOnly = 
+                TextBox5.ReadOnly = TextBox7.ReadOnly = TextBox6.ReadOnly = true;
+            DropDownList1.Enabled = DropDownList2.Enabled = Button1.Enabled = false;
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AddToGrid(object sender, EventArgs e)
+        {
+            if (DropDownList2.SelectedValue == "")
+            {
+                return;
+            }
+            SqlCommand cmd = new SqlCommand("usp_Inserting_Into_Grid", sqlConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if (TextBox1.Visible)
+                cmd.Parameters.AddWithValue("@Bill_No", TextBox1.Text);
+            else
+                cmd.Parameters.AddWithValue("@Bill_No", DropDownList3.SelectedValue);
+
+            cmd.Parameters.AddWithValue("@InvestId", DropDownList2.SelectedValue);
+
+            sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+            ShowGrid();
+        }
+
+        void ShowGrid()
+        {
+            if (DropDownList3.SelectedValue != "")
+            {
+                GridView1.Visible = true;
+                SqlDataAdapter adp = null;
+                if (TextBox1.Visible)
+                    adp = new SqlDataAdapter("exec usp_ShowGrid " + TextBox1.Text, sqlConnection);
+                else
+                    adp = new SqlDataAdapter("exec usp_ShowGrid " + DropDownList3.SelectedValue, sqlConnection);
+                sqlConnection.Open();
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+                sqlConnection.Close();
+                //if (dt.Rows.Count == 0)  
+                //{
+                //    dt.Rows.Add(dt.NewRow()); // Add a dummy row
+                //}
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            //if (e.Row.RowType == DataControlRowType.DataRow)
+            //{
+            //    // Check if this is the dummy row
+            //    if (e.Row.Cells[0].Text == "&nbsp;")
+            //    {
+            //        e.Row.Visible = false;
+            //    }
+            //}
+
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                Label lblSerialNumber = (Label)e.Row.FindControl("lblSerialNumber");
+                lblSerialNumber.Text = (e.Row.RowIndex + 1).ToString();
+            }
+
+
+        }
+
+        protected void Add(object sender, EventArgs e)
+        {
+
+            EnableTags();
+            Button2.Enabled = false;
+            Button4.Enabled = true;
+            int lastRecordBillId = GetLastBillId();
+            TextBox1.Text = (lastRecordBillId + 1).ToString();
+
+        }
+        private int GetLastBillId()
+        {
+            int lastBillId = 0;
+            
+            string query = "SELECT ISNULL(MAX(Bill_No), 0) FROM Hospital_Bill";
+            using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+            {
+                sqlConnection.Open();
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                {
+                    lastBillId = Convert.ToInt32(result);
+                }
+                sqlConnection.Close();
+            }
+            return lastBillId;
+        }
+
+        protected void Edit(object sender, EventArgs e)
+        {
+            Button2.Enabled = false;
+            Button4.Enabled = true;
+            TextBox1.Visible = false;
+            DropDownList3.Visible = true;
+            
+            sqlConnection.Open();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from Hospital_Bill", sqlConnection);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            DropDownList3.DataSource = dataTable;
+            DropDownList3.DataTextField = "Bill_No";
+            DropDownList3.DataBind();
+            sqlConnection.Close();
+
+            if (DropDownList3.SelectedValue != "0")
+            {
+                EnableTags();
+                int billId = int.Parse(DropDownList3.SelectedValue);
+                DisplayBillDetails(billId);
+                ShowGrid();
+            }
+        }
+
+
+        protected void ddlBillIDs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList3.SelectedValue != "0")
+            {
+                EnableTags();
+                int billId = int.Parse(DropDownList3.SelectedValue);
+                DisplayBillDetails(billId);
+                ShowGrid();
+            }
+        }
+        void DisplayBillDetails(int billId)
+        {
+            string fetchQuery = "select * from Hospital_Bill where Bill_No =" + billId;
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(fetchQuery, sqlConnection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                TextBox2.Text = Convert.ToDateTime(reader["Bill_Date"]).ToString("yyyy-MM-dd");
+                TextBox3.Text = reader["PatientName"].ToString();
+                DropDownList1.SelectedValue = reader["Gender"].ToString();
+                TextBox4.Text = Convert.ToDateTime(reader["DOB"]).ToString("yyyy-MM-dd");
+                TextBox5.Text = reader["Address"].ToString();
+                TextBox7.Text = reader["Email_Id"].ToString();
+                TextBox6.Text = reader["Mobile_No"].ToString();
+            }
+            sqlConnection.Close();
+        }
+
+        protected void Save(object sender, EventArgs e)
+        {
+            Console.WriteLine("hvbzkcnbdvS");
+            using (SqlCommand cmd = new SqlCommand("usp_InsertBill", sqlConnection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (TextBox1.Visible) {
+                    Console.WriteLine(TextBox1.Text);
+                    cmd.Parameters.AddWithValue("@Bill_No", TextBox1.Text);
+                }
+                else
+                {
+                    Console.WriteLine(DropDownList3.SelectedValue);
+                    cmd.Parameters.AddWithValue("@Bill_No", DropDownList3.SelectedValue);
+                }
+
+                cmd.Parameters.AddWithValue("@Bill_Date", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@PatientName", TextBox3.Text);
+                cmd.Parameters.AddWithValue("@Gender", DropDownList1.Text);
+                cmd.Parameters.AddWithValue("@DOB", TextBox4.Text);
+                cmd.Parameters.AddWithValue("@Address", TextBox5.Text);
+                cmd.Parameters.AddWithValue("@Email_Id", TextBox7.Text);
+                cmd.Parameters.AddWithValue("@Mobile_No", TextBox6.Text);
+                sqlConnection.Open();
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert ('Insertion data is succuss');", true);
+                ClearFields();
+                Button4.Enabled = false;
+                Button2.Enabled = true;
+                TextBox1.Visible = true;
+                DropDownList3.Visible = false;
+                DisableTags();
+            }
+        }
+        void ClearFields()
+        {
+            TextBox1.Text = TextBox2.Text = TextBox3.Text = TextBox4.Text =
+                TextBox5.Text = TextBox7.Text = TextBox6.Text = TextBox8.Text = "";
+            DropDownList1.SelectedValue = "";
+            //DropDownList2.SelectedValue = "";
+            GridView1.Visible=false;
+            FillDrop();
+        }
+        protected void Clear(object sender, EventArgs e)
+        {
+            TextBox1.Visible = true;
+            DropDownList3.Visible = false;
+            Button2.Enabled = true;
+            Button4.Enabled = false;
+            ClearFields();
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("select Fees from Investigation where Invest_Id =" + DropDownList2.SelectedValue, sqlConnection);
+            SqlDataReader sqlData = cmd.ExecuteReader();
+            if (sqlData.Read())
+            {
+                TextBox8.Text = sqlData.GetDecimal(0).ToString();
+            }
+            else
+            {
+                TextBox8.Text = "";
+            }
+            sqlConnection.Close();
+        }
+    }
+}
